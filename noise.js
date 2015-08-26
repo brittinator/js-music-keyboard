21df04121a46490b57f4f83d411363242933c984
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-25 21:36:16 -0700

    WIP: keys have audio.

--- noise.js
+++ noise.js
@@ -1,46 +1,50 @@
 $(document).ready( function() {
 
-    $(".note.a").click(function () {
-      var audio_tag = document.getElementById('aAudio'); // audio file
-      audio_tag.currentTime = 0;
-      audio_tag.play();  // command to play audio
-    });
+  $(".note.a").click(function () {
+    var audio_tag = document.getElementById('aAudio'); // audio file
+    audio_tag.currentTime = 0;
+    audio_tag.play();  // command to play audio
+  });
 
-    $(".note.b").click(function () {
-      var audio_tag = document.getElementById('bAudio'); // audio file
-      audio_tag.currentTime = 0;
-      audio_tag.play();  // command to play audio
-    });
+  $(".note.b").click(function () {
+    var audio_tag = document.getElementById('bAudio'); // audio file
+    audio_tag.currentTime = 0;
+    audio_tag.play();  // command to play audio
+  });
 
-    $(".note.c").click(function () {
-      var audio_tag = document.getElementById('cAudio'); // audio file
-      audio_tag.currentTime = 0;
-      audio_tag.play();  // command to play audio
-    });
+  $(".note.c").click(function () {
+    var audio_tag = document.getElementById('cAudio'); // audio file
+    audio_tag.currentTime = 0;
+    audio_tag.play();  // command to play audio
+  });
 
-    $(".note.d").click(function () {
-      var audio_tag = document.getElementById('dAudio'); // audio file
-      audio_tag.currentTime = 0;
-      audio_tag.play();  // command to play audio
-    });
+  $(".note.d").click(function () {
+    var audio_tag = document.getElementById('dAudio'); // audio file
+    audio_tag.currentTime = 0;
+    audio_tag.play();  // command to play audio
+  });
 
-    $(".note.e").click(function () {
-      var audio_tag = document.getElementById('eAudio'); // audio file
-      audio_tag.currentTime = 0;
-      audio_tag.play();  // command to play audio
-    });
+  $(".note.e").click(function () {
+    var audio_tag = document.getElementById('eAudio'); // audio file
+    audio_tag.currentTime = 0;
+    audio_tag.play();  // command to play audio
+  });
 
-    $(".note.f").click(function () {
-      var audio_tag = document.getElementById('fAudio'); // audio file
-      audio_tag.currentTime = 0;
-      audio_tag.play();  // command to play audio
-    });
+  $(".note.f").click(function () {
+    var audio_tag = document.getElementById('fAudio'); // audio file
+    audio_tag.currentTime = 0;
+    audio_tag.play();  // command to play audio
+  });
 
-    $(".note.g").click(function () {
-      var audio_tag = document.getElementById('gAudio'); // audio file
-      audio_tag.currentTime = 0;
-      audio_tag.play();  // command to play audio
-    });
+  $(".note.g").click(function () {
+    var audio_tag = document.getElementById('gAudio'); // audio file
+    audio_tag.currentTime = 0;
+    audio_tag.play();  // command to play audio
+  });
+
+  // $(".note.g").keydown(function(event){
+  //   alert("Test");
+  // });
 
 });
 
